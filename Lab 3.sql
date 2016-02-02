59df6d328e4125a54c6ce77dbdd57157bccd10a3
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-02 16:12:43 -0500

    second question

--- Lab 3.sql
+++ Lab 3.sql
@@ -1,7 +1,11 @@
 ﻿-- Morgan Stippa 2/2/15
 -- Lab 3
 
---lists the order number and dollars of all orders
+-- 1. lists the order number and dollars of all orders
 select ordnum, totalusd
 from orders;
 
+-- 2. Lists the name and city of the agents Smith
+select name, city
+from agents
+where name = 'Smith';

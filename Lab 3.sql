c0c4a85081468f0fd9883b3c42641c0d3cc1fb5d
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-02 16:23:32 -0500

    third question

--- Lab 3.sql
+++ Lab 3.sql
@@ -2,10 +2,18 @@
 -- Lab 3
 
 -- 1. lists the order number and dollars of all orders
-select ordnum, totalusd
+select ordnum, totalUSD
 from orders;
 
 -- 2. Lists the name and city of the agents Smith
 select name, city
 from agents
 where name = 'Smith';
+
+-- 3. Lists the pid, name, and priceUSD of products with quantity more than 208,000
+select pid, name, priceUSD
+from products
+where (quantity > 208000);
+
+
+

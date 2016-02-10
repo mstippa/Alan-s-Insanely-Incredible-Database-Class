f89a2cd9a6dc9ed50d966a609cac9aef7d313740
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-10 09:14:00 -0500

    question 1

---
+++ Lab 4.sql
@@ -0,0 +1,12 @@
+﻿-- Morgan Stippa 2/10/15
+-- Lab 4
+
+-- 1. Gets the cities of agents booking an order for a customer whose cid is 'c002'
+select city
+from agents
+where aid in (select aid
+	      from orders
+	      where cid = 'c002'
+	      );
+
+

f1610bdfb5022cf30b2c6218b3c472e427507db1
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-11 12:59:04 -0500

    question 3, 4, 5, and 6, 3 needs to be fixed

--- Lab 4.sql
+++ Lab 4.sql
@@ -13,16 +13,48 @@ where aid in (select aid
 -- 2. Gets the ids of products ordered	through	any agent who takes at	least one order	from a	
 --    customer in Dallas, sorted by pid from highest to lowest.
 select pid
-from products
-where pid in (select pid
+from orders
+where cid in (select cid
+              from customers
+              where city = 'Dallas'
+             )
+order by pid desc;       
+
+-- 3. Gets the ids and names of customers who did not place an order through agent a01                  
+select cid, name
+from customers
+where cid in (select distinct cid
 	      from orders
-	      where cid in (select cid
-                            from customers
-                            where city = 'Dallas'
-                           ))
-order by pid DESC;                            
-			
-	
+	      where aid <> 'a01'
+	     ); 
+
+-- 4. Gets the ids of customers who ordered both product p01 and p07 
+select cid 
+from orders
+where pid = 'p01'
+  intersect
+select cid
+from orders 
+where pid = 'p07';
+
+-- 5. Gets the ids of products not ordered by any customers who placed any order through	
+--    agent a07	in pid order from highest to lowest
+select pid
+from orders
+where aid <> 'a07'
+order by pid desc;
+
+-- 6. Gets the name, discounts, and city for all customers who place orders through agents	
+--    in London	or New York 
+select name, city, discount
+from customers
+where cid in (select cid 
+	      from orders
+	      where aid in (select aid
+			    from agents
+			    where city = 'London'
+				or city = 'New York'
+			    ));
 
 
 

c95e917c33fe95e995c52890689191b485b482f1
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-17 11:06:33 -0500

    quesiton 1_

---
+++ Lab_5.sql
@@ -0,0 +1,9 @@
+﻿-- Morgan Stippa 2/17/15
+-- Lab 5
+
+-- 1. Show the cities of agents booking an order for a	customer whose	id is 'c002'. Use	
+ --   joins; no	subqueries.	
+ select city
+ from agents, orders
+ where orders.aid = agents.aid
+	and cid = 'c002';

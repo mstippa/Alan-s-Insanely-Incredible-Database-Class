ac9be2c67e193139ec6dab952a1286c18bf02ebc
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-17 11:18:59 -0500

    question 2

--- Lab_5.sql
+++ Lab_5.sql
@@ -2,8 +2,17 @@
 -- Lab 5
 
 -- 1. Show the cities of agents booking an order for a	customer whose	id is 'c002'. Use	
- --   joins; no	subqueries.	
+--   joins; no	subqueries.	
  select city
  from agents, orders
  where orders.aid = agents.aid
 	and cid = 'c002';
+
+-- 2. Show the ids of products ordered through any agent who makes at least one order for 
+--    for a customer in Dallas, sorted by pid from higest to lowest. Use joins
+select pid
+from  orders o, customers c
+where c.cid = o.cid
+	and c.city = 'Dallas'
+order by pid desc;
+

0ce2012e3fe29230d6a1bc06fe857c6a65a10d58
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-25 16:20:53 -0500

    questions 4 and 5

--- Lab_6/Lab_6.sql
+++ Lab_6/Lab_6.sql
@@ -22,6 +22,35 @@ where priceUSD > (select avg(priceUSD)
 		  from products
 		  ); 
 
+-- 3. Display the customer name, pid ordered, and the	total for all orders, sorted by total	
+--    from high	to low.	
+select customers.name, products.pid, orders.totalUSD
+from orders inner join customers on orders.cid = customers.cid
+            inner join products on orders.pid = products.pid
+order by totalUSD desc;
+
+-- 4. Display all customer names (in alphabetical order) and their total ordered, and	
+--    nothing more. Use coalesce to avoid showing NULLs.
+select customers.name, sum(totalUSD)
+from orders inner join customers on orders.cid = customers.cid
+group by customers.cid
+order by customers.name asc;
+
+-- 5. Display the names of all customers who bought products from agents based in Tokyo
+--    along with the names of the products they ordered, and	the names of the agents who	
+--    sold it to them
+select customers.name, products.name, agents.name
+from orders inner join customers on orders.cid = customers.cid
+	    inner join products on orders.pid = products.pid
+	    inner join agents on orders.aid = agents.aid
+where agents.city = 'Tokyo'	    
+
+-- 6. Write a query to	check the accuracy of	the dollars column in the Orders table. This	
+--    means calculating Orders.totalUSD from data in other tables and	comparing those	
+--    values to	the values in Orders.totalUSD.  Display all rows in Orders where	
+--    Orders.totalUSD is incorrect, if	any
+
+	    
 
 
 

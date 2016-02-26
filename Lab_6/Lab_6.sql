7f937a8aadb37e8b923db2b57593d0aa5a668f96
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-02-25 19:45:32 -0500

    questions 6 and 7

--- Lab_6/Lab_6.sql
+++ Lab_6/Lab_6.sql
@@ -43,13 +43,34 @@ select customers.name, products.name, agents.name
 from orders inner join customers on orders.cid = customers.cid
 	    inner join products on orders.pid = products.pid
 	    inner join agents on orders.aid = agents.aid
-where agents.city = 'Tokyo'	    
+where agents.city = 'Tokyo';	    
 
 -- 6. Write a query to	check the accuracy of	the dollars column in the Orders table. This	
 --    means calculating Orders.totalUSD from data in other tables and	comparing those	
 --    values to	the values in Orders.totalUSD.  Display all rows in Orders where	
 --    Orders.totalUSD is incorrect, if	any
+select *
+from orders inner join customers on orders.cid = customers.cid
+	    inner join products on orders.pid = products.pid
+where orders.totalUSD <> ((products.priceUSD * orders.qty) - (products.priceUSD * orders.qty * (customers.discount/100)));	    
 
+-- 7. What’s the difference between a LEFT OUTER JOIN	and a RIGHT OUTER JOIN? Give	
+--    example queries in SQL to demonstrate. (Feel free to use the CAP2 database to make	
+--    your points here.)
+
+-- Unlike inner joins, outer joins return all rows from at least one row, whether or not there is a row
+-- from both tables that mathches the condition of the join. A left outer join returns all rows from the left table
+-- whether or not there is a corresponding row from the right table. Here's an example:
+select *
+from orders left outer join customers on orders.cid = customers.cid; 
+-- No null values are returned from the right table because for every cid in the orders table, there is a matching cid in the customers
+-- table. A right outer join returns all rows from the right tables whether or not there is a corresponding row
+-- from the left table. Example:
+select *
+from orders right outer join customers on orders.cid = customers.cid;
+-- Null values are returned from the left table because there is a cid from the customers table that doesn't
+-- match any from the orders table. So all the rows are displayed from the customers table, even the one that
+-- doesn't match any row from the orders table.
 	    
 
 

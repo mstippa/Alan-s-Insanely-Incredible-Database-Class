ffe498786e6026214c60c3f66b2405376662a98b
Morgan Stippa <Morgan.Stippa1@marist.edu>
2016-04-11 22:06:46 -0400

    adding another version of the script but as sql

---
+++ Lab 9/lab 9 scripts.sql
@@ -0,0 +1,63 @@
+﻿-- People --
+create table people (
+  pid		char(4) not null,
+  firstName	text,
+  lastName	text,
+  ZIP		integer,
+  state		text,
+  city		text,
+  phoneNumber	integer,
+ primary key(pid)
+);
+
+-- head coaches --
+create table headCoaches (
+  hid		char(4) not null references people(pid),
+ primary key(hid)
+); 
+
+-- teams --
+create table teams (
+  tid 		char(5) not null,
+  ageGroup	numeric(1),  		   
+  hid		char(4) references headCoaches(hid),
+  teamName 	text,
+ primary key(tid),
+ constraint head_coaches unique(hid, ageGroup)
+);  
+
+-- players --
+create table players (
+  pid 		char(4) not null references people(pid),
+  tid		char(5) references teams(tid),
+ primary key(pid)
+);
+
+-- coaches --
+create table coaches (
+  pid		char(4) not null references people(pid),
+  yearsCoached	integer,
+ primary key(pid)
+);
+
+-- assistant coaches --
+create table assistantCoaches (
+  aid		char(4) not null references people(pid),
+ primary key(aid)
+);
+
+-- teams assistant coaches --
+create table teamsAssistantCoaches (
+  tid		char(5) not null references teams(tid),
+  aid		char(4) not null references assistantCoaches(aid), 
+  ageGroup	numeric(1), 
+ primary key(tid, aid),
+ constraint assistant_coaches unique (aid, ageGroup)
+);
+
+
+
+
+
+
+     
